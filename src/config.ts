1cdc869c080edc9e9c7e430ee3abf0248c819edd
yushunqwq <yushun080814@gmail.com>
2025-08-17 22:20:52 +0800

    项目初始化

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Fuwari",
+	title: "yushunqwq",
 	subtitle: "Demo Site",
 	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
